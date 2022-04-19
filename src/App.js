6637a64a66d282c924a1662b378f393a2f1facf0
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-04-19 01:18:37 -0300

    adiciona função que muda a rota

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loginButtonDisable: true,
+      isLogin: false,
+    };
+  }
+
+  ChangeState = ({ target }) => {
+    const three = 3;
+    const { value, name } = target;
+    if (name === 'userName') { this.setState({ userName: value }); }
+    const btnCheck = value.length < three;
+    this.setState({ loginButtonDisable: btnCheck });
+  }
+
+  User = () => {
+    const { userName } = this.state;
+    createUser({ name: userName });
+    this.setState({ isLogin: true });
+    // isLogin ? (<Redirect to="/search" />) : (<Login />);
+  }
+
+  //--------------------------------------------------------------------------------
   render() {
+    const { loginButtonDisable, userName, isLogin } = this.state;
     return (
       <div>
         <p>TrybeTunes!</p>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={ Login } />
+            { isLogin && <Redirect to="/search" /> }
+            <Route
+              exact
+              path="/"
+              render={ () => (<Login
+                buttonDisabled={ loginButtonDisable }
+                value={ userName }
+                funct={ this.ChangeState }
+                name="userName"
+                btnFunct={ this.User }
+              />) }
+            />
+
             <Route path="/search" component={ Search } />
             <Route path="/favorites" component={ Favorites } />
             <Route path="/profile" component={ Profile } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-// import { BrouserRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  // UserUpdate = () => );
+
   render() {
+    const { buttonDisabled, value, name, funct, btnFunct } = this.props;
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <form action="">
+
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ value }
+            name={ name }
+            onChange={ funct }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ buttonDisabled }
+            // onClick={ () => createUser({ name: value }) }
+            onClick={ btnFunct }
+          >
+            Entrar
+          </button>
+
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  buttonDisabled: PropTypes.bool.isRequired,
+  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  funct: PropTypes.func.isRequired,
+  btnFunct: PropTypes.func.isRequired,
+};
 
 export default Login;
