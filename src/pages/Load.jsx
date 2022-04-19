1f9d34f89b96bf402eaf95811e79069e1f9d281f
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-04-19 20:38:16 -0300

    adiciona requisito 2

---
+++ src/pages/Load.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Load extends Component {
+  render() {
+    return <h1 className="load">Carregando...</h1>;
+  }
+}
+
+export default Load;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,78 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { createUser } from '../services/userAPI';
+import Load from './Load';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
-  // UserUpdate = () => );
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loginButtonDisable: true,
+      // isLogin: false,
+      isLoading: false,
+    };
+  }
+
+  loadOff = () => {
+    this.setState({ isLoading: false });
+  }
+
+  ChangeState = ({ target }) => {
+    const three = 3;
+    const { value, name } = target;
+    if (name === 'userName') { this.setState({ userName: value }); }
+    const btnCheck = value.length < three;
+    this.setState({ loginButtonDisable: btnCheck });
+  }
+
+  logIn = async () => {
+    const { userName } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name: userName });
+    history.push('/search');
+  }
 
   render() {
-    const { buttonDisabled, value, name, funct, btnFunct } = this.props;
+    const { loginButtonDisable, userName, isLoading } = this.state;
     return (
-      <div data-testid="page-login">
-        <h1>Login</h1>
-        <form action="">
 
-          <input
-            type="text"
-            data-testid="login-name-input"
-            value={ value }
-            name={ name }
-            onChange={ funct }
-          />
-          <button
-            type="button"
-            data-testid="login-submit-button"
-            disabled={ buttonDisabled }
-            // onClick={ () => createUser({ name: value }) }
-            onClick={ btnFunct }
-          >
-            Entrar
-          </button>
+      isLoading ? (
+        <Load />
+      ) : (
 
-        </form>
-      </div>
+        <div data-testid="page-login">
+          <h1>Login</h1>
+          <form action="">
+
+            <input
+              type="text"
+              data-testid="login-name-input"
+              value={ userName }
+              name="userName"
+              onChange={ this.ChangeState }
+              placeholder="Nome do Usuario"
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ loginButtonDisable }
+              onClick={ this.logIn }
+            >
+              Entrar
+            </button>
+
+          </form>
+        </div>
+      )
+
     );
   }
 }
 
 Login.propTypes = {
-  buttonDisabled: PropTypes.bool.isRequired,
-  value: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  funct: PropTypes.func.isRequired,
-  btnFunct: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(Object).isRequired,
 };
 
 export default Login;
