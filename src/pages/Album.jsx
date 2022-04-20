4d3c3434d367970c954b0c1e9491939494d5ca14
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-04-20 18:48:27 -0300

    adiciona requisito 6

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 import Header from '../components/Header';
+// import getMusics from '../services/musicsAPI';
+import './style.css';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      album: '',
+    };
+  }
+
+  async componentDidMount() {
+    // const { match: { params: { id } } } = this.props;
+    // const find = id.split('-');
+    // const albums = await getMusics(id);
+    // const album = albums.find((alb) => alb.collectionId === Number(find[1]));
+    // this.setState({ album });
+  }
+
   render() {
+    const { album } = this.state;
     return (
       <div data-testid="page-album">
         <h2>Album</h2>
         <Header />
+        <section className="album-view">
+          <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+          <p>{ album.collectionName }</p>
+          <p>{ album.artistName }</p>
+          <Link
+            to="/search"
+          >
+            Voltar
+          </Link>
+        </section>
       </div>
     );
   }
 }
+
+// Album.propTypes = {
+//   match: PropTypes.objectOf(Object).isRequired,
+// };
 
 export default Album;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Load from './Load';
+// import Album from './Album';
+import './style.css';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       findArtist: '',
+      artistName: '',
       findButtonDisable: true,
+      isLoading: false,
+      albums: [],
     };
   }
 
@@ -18,35 +26,78 @@ class Search extends Component {
     this.setState({ findButtonDisable: btnCheck });
   }
 
-  SearchArtist = () => {}
+  SearchArtist = async () => {
+    const { findArtist } = this.state;
+    this.setState({ isLoading: true });
+    this.setState({ artistName: findArtist });
 
+    const artists = await searchAlbumsAPI(findArtist);
+    this.setState({ albums: artists });
+    this.setState({ findArtist: '' });// ultima <=
+    this.setState({ findButtonDisable: true });
+    this.setState({ isLoading: false });
+    console.log(artists);
+  }
+
+  //  -------------------------------------------------------------------------------
   render() {
-    const { findArtist, findButtonDisable } = this.state;
+    const { findArtist, findButtonDisable, isLoading, artistName, albums } = this.state;
+
     return (
-      <div data-testid="page-search">
-        <h2>Search</h2>
-        <Header />
-        <form action="">
+      isLoading ? (<Load /> // condicional do loading
+      ) : (
+        <div data-testid="page-search">
+          <h2>Search</h2>
+          <Header />
+          { isLoading ? ( // condicional para esconder botao e input
+            <p> </p>
+          ) : (
+            <form action="">
 
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            value={ findArtist }
-            name="findArtist"
-            onChange={ this.ChangeState }
-            placeholder="Nome do Artista"
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ findButtonDisable }
-            onClick={ this.SearchArtist }
-          >
-            Procurar
-          </button>
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                value={ findArtist }
+                name="findArtist"
+                onChange={ this.ChangeState }
+                placeholder="Nome do Artista"
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ findButtonDisable }
+                onClick={ this.SearchArtist }
+              >
+                Procurar
+              </button>
+            </form>
+          )}
+          {artistName && (
+            <p>
+              Resultado de álbuns de:
+              { ` ${artistName}` }
+            </p>)}
 
-        </form>
-      </div>
+          <section className="album-contain">
+            { albums.length === 0 ? (<h3>Nenhum álbum foi encontrado</h3>
+            ) : (
+              albums.map((alb) => (
+                <div key={ alb.collectionId } className="album-box">
+                  <img src={ alb.artworkUrl100 } alt={ alb.collectionName } />
+                  <p>{ alb.collectionName }</p>
+                  <p>{ alb.artistName }</p>
+                  <Link
+                    to={ `/album/${alb.collectionId}` }
+                    data-testid={ `link-to-album-${alb.collectionId}` }
+                  >
+                    Detalhes
+                  </Link>
+                </div>
+              ))
+            )}
+
+          </section>
+        </div>)
     );
   }
 }
